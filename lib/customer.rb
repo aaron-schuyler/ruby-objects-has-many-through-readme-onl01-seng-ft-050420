93d1e6689fe4a99e50aad4e6d9b3cc68ba474517
Aaron Schuyler <asky28@gmail.com>
2020-05-20 00:32:08 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -12,4 +12,9 @@ class Customer
   def new_meal(customer, total, tip=0)
     Meal.new(waiter, self, total, tip)
   end
+  def meals
+    Meal.all.select do |meal|
+      meal.customer == self
+    end
+  end
 end
